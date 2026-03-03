8d3dd7805692414cee73578cf4cd772248f0cc71
Polar-D-Penguin <148401073+PolarBearFace@users.noreply.github.com>
2026-03-03 14:11:14 -0500

    canvas setup

--- GameFiles/code.js
+++ GameFiles/code.js
@@ -40,3 +40,16 @@ class Unit{
         this.stats = unitStats.type
     }
 }
+
+const gameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 440;
+        this.canvas.height = 680;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+function startGame(){
+    gameArea.start();
+}
